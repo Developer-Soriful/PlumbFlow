63f7122b6c79394ca196b91caad8857377f9bc28
SabbirHossainEvan <evansabbir31@gmail.com>
2026-04-20 16:43:05 +0600

    Add AboutSection component and related data for company information

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { HeroSection } from "@/components/sections/hero-section";
+import { AboutSection } from "@/components/sections/about-section";
 import { QuickBookingSection } from "@/components/sections/quick-booking-section";
 import { Navbar } from "@/components/layout/navbar";
 import { TopBar } from "@/components/layout/top-bar";
@@ -12,6 +13,7 @@ export default function Home() {
         <HeroSection />
       </div>
       <QuickBookingSection />
+      <AboutSection />
     </main>
   );
 }
